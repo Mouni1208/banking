0219110f0a25d057b9c9e7b5d5c94e41939e7109
MOUNI78 <battulamounika78@gmail.com>
2024-09-29 12:14:26 +0530

    first home page

--- banking.app/src/main/java/net/javaguides/banking/entity/Account.java
+++ banking.app/src/main/java/net/javaguides/banking/entity/Account.java
@@ -18,11 +18,12 @@ public class Account {
 
 
     @Id
-
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "account_holder_name")
     private String accountHolderName;
+
+    @Column(name = "balance")
     private double balance;
 }
